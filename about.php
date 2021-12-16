118b835778eb10a241f7b7dc68cbe53ff71c7ccf
Namasake20 <calebmasake69@gmail.com>
2021-12-16 13:35:27 +0300

    initial commit

---
+++ about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RentARide | About</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	    <link rel="stylesheet" type="text/css" href="style.css">
+	<?php include("nav.php") ?>
+</head>
+<body>
+	<section id="about" class="about section-bg">
+		<div class="container">
+			<div class="section-title">
+				<h2>ABOUT US</h2>
+				
+            </div>
+
+            <div class="row">
+            	<center><h3>About Us</h3></center>
+            	<div  class="col-6">
+            		RentARide aims at providing the best services in transport.
+            		
+            		
+            	</div>
+            	<div class="col-6">
+            		<p>
+            			
+            			<div class="card" id="card1" >
+            			<p style="padding: 10px" >
+            				CONTACT INFO & DETAILS<br>
+            				8:00AM – 6:00PM<br>
+            				(+254) 7....<br>
+            				rentaride@gmail.com<br>
+            				Westlands, Nairobi, Kenya
+                        </p>
+            			
+            		</div>
+            		</p>
+            		
+            	</div>
+            	
+            </div>
+			
+		</div>
+		
+	</section>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,27 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.php">RentARide</a></h1>
+      
+
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">HOME</a></li>
+          <li><a class="nav-link scrollto " href="about.php">ABOUT</a></li>
+          
+          </li>
+          <li><a class="nav-link scrollto " href="privacy.php">PRIVACY POLICY</a></li>
+          <li><a class="nav-link scrollto " href="conditions.php">TERMS AND CONDITIONS</a></li>
+          
+          </li>
+        
+          <li><a class="nav-link scrollto" href="contact.php">CONTACT</a></li>
+          
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+</header>
